5ea3a1024f903073eacafc3bad21f9d4e6da7f71
zijian <zijian.cheng@2339.com>
2017-08-26 18:15:11 +0800

    [update] add db

--- app/src/main/java/com/android/zj/ai/MainActivity.java
+++ app/src/main/java/com/android/zj/ai/MainActivity.java
@@ -21,11 +21,17 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.zj.ai.db.DaoMaster;
+import com.android.zj.ai.db.MySQLiteOpenHelper;
+import com.android.zj.ai.db.PhoneData;
+import com.android.zj.ai.db.PhoneDataDao;
 import com.android.zj.ai.utils.AppUtils;
 import com.android.zj.ai.utils.CacheUtil;
 import com.android.zj.ai.utils.SuUtil;
+import com.github.yuweiguocn.library.greendao.MigrationHelper;
 
 import java.lang.reflect.Method;
+import java.util.Date;
 
 import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
@@ -34,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private boolean isBegin;
+    private DaoMaster daoMaster;
+    private PhoneDataDao mPhoneDataDao;
     private TextView tv_catcheSize;
 
     @Override
@@ -41,13 +49,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_catcheSize = (TextView) findViewById(R.id.tv_catcheSize);
+
+        MigrationHelper.DEBUG = true;
+        MySQLiteOpenHelper helper = new MySQLiteOpenHelper(this, "test.db", null);
+        daoMaster = new DaoMaster(helper.getEncryptedWritableDb("12342"));
+        mPhoneDataDao = daoMaster.newSession().getPhoneDataDao();
+
         findViewById(R.id.id_test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                SuUtil.kill("com.android.daoway");
 //                CacheUtil.delCacheFile();
 
-                SuUtil.kill("com.chengzj.zd", false);
+//                SuUtil.kill("com.chengzj.zd", false);
+                initDB();
             }
         });
 
@@ -75,102 +89,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initDB() {
+        PhoneData test = new PhoneData(
+                "960",
+                "540",
+                "G22+X715E+RUBY/AMAZE 4G",
+                "",
+                "HTC",
+                "2.3",
+                "35726504",
+                "HTC X715e（Amaze 4G/G22）"
+        );
+        mPhoneDataDao.insert(test);
+
+        //mPhoneDataDao.loadByRowId(0);
+        Log.d("MigrationHelper", "TestData2 " + mPhoneDataDao.loadAll().toString());
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
-        if (isBegin) {
-            if (AppUtils.isAccessibilitySettingsOn(MainActivity.this)) {
-                AppUtils.openApp();
-            } else {
-                Toast.makeText(MainActivity.this, "请开启服务，否则无法运行", Toast.LENGTH_LONG).show();
-            }
-            isBegin = false;
-        }
     }
 
-    private final int MSG_GET_DATASIZE = 0x00;
-    private final int MSG_CLEAR_DATA_SUCCESS = 0x01;
-    private long catcheSize;
-
-    private Handler handler = new Handler() {
-        public void handleMessage(android.os.Message msg) {
-            switch (msg.what) {
-                case MSG_GET_DATASIZE:
-                    String size = Formatter.formatFileSize(MainActivity.this, catcheSize);
-                    tv_catcheSize.setText(size);
-                    break;
-                case MSG_CLEAR_DATA_SUCCESS:
-                    String pkgname = (String) msg.obj;
-                    queryPacakgeSize(pkgname);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        ;
-    };
-
-    public void queryPacakgeSize(String pkgName) {
-        if (pkgName != null) {
-            //使用放射机制得到PackageManager类的隐藏函数getPackageSizeInfo
-            PackageManager pm = getPackageManager();  //得到pm对象
-            try {
-                //通过反射机制获得该隐藏函数
-                Method getPackageSizeInfo = pm.getClass().getDeclaredMethod("getPackageSizeInfo", String.class, IPackageStatsObserver.class);
-                //调用该函数，并且给其分配参数 ，待调用流程完成后会回调PkgSizeObserver类的函数
-                getPackageSizeInfo.invoke(pm, pkgName, statsObserver);
-            } catch (Exception ex) {
-                Log.e(TAG, "NoSuchMethodException");
-                ex.printStackTrace();
-            }
-        }
-    }
-
-//    private void getSize(String packageName) {
-//        if (!TextUtils.isEmpty(packageName)) {
-//            PackageManager pManager = getPackageManager();
-//            pManager.getPackageSizeInfo(packageName, statsObserver);
-//        }
-//    }
-
-    IPackageStatsObserver statsObserver = new IPackageStatsObserver.Stub() {
-        @Override
-        public void onGetStatsCompleted(PackageStats pStats, boolean succeeded)
-                throws RemoteException {
-            // TODO Auto-generated method stub
-            catcheSize = pStats.dataSize;
-            handler.sendEmptyMessage(MSG_GET_DATASIZE);
-        }
-    };
-
-    class ClearUserDataObserver extends IPackageDataObserver.Stub {
-        public void onRemoveCompleted(final String packageName, final boolean succeeded) {
-            Log.d(TAG, "packageName " + packageName + "   succeeded  " + succeeded);
-            if (succeeded) {
-                Message msg = Message.obtain();
-                msg.what = MSG_CLEAR_DATA_SUCCESS;
-                msg.obj = packageName;
-                handler.sendMessage(msg);
-            }
-        }
-    }
-
-    private ClearUserDataObserver mClearDataObserver;
-
-    public void clear(String packageName) {
-        if (mClearDataObserver == null) {
-            mClearDataObserver = new ClearUserDataObserver();
-        }
-        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        try {
-            Class<?> amClass = Class.forName(am.getClass().getName());
-            Method clearApp = amClass.getMethod("clearApplicationUserData", String.class, IPackageDataObserver.class);
-            Log.d("MainActivity", "clearApp: " + clearApp.getName());
-            clearApp.invoke(am, packageName, mClearDataObserver);
-        } catch (Exception e) {
-            Log.d("MainActivity", "Exception: " + e.toString());
-            e.printStackTrace();
-        }
+    @Override
+    protected void onDestroy() {
+        mPhoneDataDao.getDatabase().close();
+        super.onDestroy();
     }
 }
